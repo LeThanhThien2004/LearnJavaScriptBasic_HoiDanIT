e43768a3395cb5a4e83dd68f8d338f45bea2249c
LeThanhThien2004 <thienlethanh2210@gmail.com>
2025-07-30 22:05:51 +0700

    video18-DataTypes

---
+++ src/video18/video18.js
@@ -0,0 +1,25 @@
+
+
+//number
+const myAge = 21
+const myScore = 9.1
+console.log("Video 18")
+
+console.log("My age is " + myAge)
+console.log("My score is " + myScore)
+
+//string
+const name = "Kindness"
+console.log("My name is " + name)
+
+//boolean: true/false
+const isStudent = true
+console.log("Is student: " + isStudent)
+
+//undefined : có nghĩ là chưa gán giá trị
+let bla
+console.log(bla)
+
+//null : có nghĩ là đã gán giá trị nhưng rỗng
+let testNull = null
+console.log(testNull)
