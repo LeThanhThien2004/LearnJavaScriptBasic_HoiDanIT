440f4e3d01c437fd9db0b005b9e30739154c7416
LeThanhThien2004 <thienlethanh2210@gmail.com>
2025-09-05 17:13:23 +0700

    Finished Chapter 7

---
+++ src/chapter07/video66/video66.js
@@ -0,0 +1,15 @@
+console.log("Video 66");
+
+fetch("http://localhost:8000/users")
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+// async await
+
+const fetchData = async() => {
+    const res = await fetch("http://localhost:8000/users")
+    const data = await res.json();
+    console.log(data);
+}
+
+fetchData();
